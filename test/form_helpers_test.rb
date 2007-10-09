6de4cea98bce6e1f2ae458f52bb8df1ca399a0da
Sergio Gil <sgilperez@gmail.com>
2007-10-09 17:10:46 +0000

    Preparing to test helpers

--- test/form_helpers_test.rb
+++ test/form_helpers_test.rb
@@ -1,7 +1,10 @@
 require 'test/unit'
 require 'rubygems'
 require 'active_record'
+require 'action_controller'
+require 'action_view'
 require File.dirname(__FILE__) + '/../lib/live_validations'
+require File.dirname(__FILE__) + '/../lib/form_helpers'
 require File.dirname(__FILE__) + '/../test/resource'
 
 class FormHelpersTest < Test::Unit::TestCase
