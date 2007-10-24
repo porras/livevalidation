82518137b3ff94d624f3ccf15c8e7e03110cd6a5
Sergio Gil <sgilperez@gmail.com>
2007-10-24 17:07:52 +0000

    Helpers testing

--- test/form_helpers_test.rb
+++ test/form_helpers_test.rb
@@ -13,6 +13,11 @@ class ResourcesController < ActionController::Base
     render :inline => "<% form_for(:resource, :url => resources_path) do |f| %><%= f.text_field :name %><% end %>"
   end
 
+  def name
+    @resource = Resource.new
+    render :inline => "<% form_for(:resource, :url => resources_path) do |f| %><%= f.text_field :name %><% end %>"
+  end
+
   def rescue_action(e)
     raise e
   end
@@ -36,7 +41,29 @@ class FormHelpersTest < Test::Unit::TestCase
 
   def test_without_instance_var
     get :without_instance_var
+    check_form_item :type => 'text', :name => 'name'
+  end
+
+  def test_presence
+    Resource.class_eval do
+      validates_presence_of :name
+    end
+    get :name
+    check_form_item :type => 'text', :name => 'name', :script => "var resource_name = new LiveValidation('resource_name');resource_name.add(Validate.Presence, null)"
+  end
+
+  private
+
+  def check_form_item(options = {})
     assert_response :ok
+    assert_select 'form[action="/resources"]' do
+      assert_select "input[type='#{options[:type]}'][id='resource_#{options[:name]}']"
+      if options[:script]
+        assert_select 'script', options[:script]
+      else
+        assert_select 'script', 0
+      end
+    end
   end
 
 end
